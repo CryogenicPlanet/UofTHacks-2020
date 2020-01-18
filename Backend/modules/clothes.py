388fd7b1c9ec1ef0e8acb83b80cbe1c08d7197e2
Ali Abdoli <xxaligatorxx@outlook.com>
2020-01-18 16:25:44 -0500

    got rid of test

--- Backend/modules/clothes.py
+++ Backend/modules/clothes.py
@@ -2,6 +2,5 @@ from modules import database, weather
 import datetime
 
 def put_classification(link, location):
-    print('test')
     time_now = datetime.datetime.now()
     database.insert_clothes(time_now, weather.get_temp(location), location, "sweater")

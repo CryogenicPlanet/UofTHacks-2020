909584709b210892b993508f4f6c93958058eff9
Ali Abdoli <xxaligatorxx@outlook.com>
2020-01-18 21:24:48 -0500

    fixed bug

--- Backend/server.py
+++ Backend/server.py
@@ -21,7 +21,7 @@ def get_clothes():
 def set_clothes():
     img_encoded = request.json['image']
     image = base64.b64decode(img_encoded)
-    fh = open("/home/rahultarak12345/UofTHacks-2020/img.jpg" + str(random.random()), "wb")
+    fh = open("/home/rahultarak12345/UofTHacks-2020/img"  + str(random.random()) + ".jpg", "wb")
     fh.write(image)
     fh.close()
     location = request.json['location']
